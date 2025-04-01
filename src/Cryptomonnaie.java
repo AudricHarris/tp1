ba85d5a8ea45814d18b2e982c0d1c92c500a064b
Audric Harris <audricharrismail@gmail.com>
2025-04-01 15:30:00 +0200

    Modification de Cryptomonnaie

--- src/Cryptomonnaie.java
+++ src/Cryptomonnaie.java
@@ -8,4 +8,19 @@ public class Cryptomonnaie
 		this.nom = nom;
 		this.valeurDeJeton = valeurDeJeton;
 	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public int getValeurDeJeton()
+	{
+		return valeurDeJeton;
+	}
+
+	public String toString()
+	{
+		return String.format("%5s",nom) + ":" + String.format("%10.1f",valeurDeJeton);
+    }
 }
