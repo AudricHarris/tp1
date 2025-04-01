911ace2c90df00acc82a738bf361a9b4d6d9cd89
Audric Harris <audricharrismail@gmail.com>
2025-04-01 14:09:25 +0200

    Ajout du fichier src

---
+++ src/Cryptomonnaie.java
@@ -0,0 +1,11 @@
+public class Cryptomonnaie 
+{
+	private String nom;
+	private int valeurDeJeton;
+
+	public Cryptomonnaie(String nom, int valeurDeJeton)
+	{
+		this.nom = nom;
+		this.valeurDeJeton = valeurDeJeton;
+	}
+}
